92d9b3aa4e003464fa5ba894848481c49b108c06
NicolasAlvesM <nicolas.morenoam@outlook.com>
2024-09-20 15:15:31 -0300

    chore: remove console log

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,3 @@
-"use client"
 import { API_URL } from '@/configs/enviromentsVars';
 import axios from 'axios';
 import Cookies from 'js-cookie';
@@ -9,7 +8,6 @@ export const api = axios.create({
 
 api.interceptors.request.use(config => {
   const token = Cookies.get('session');
-  console.log({ token });
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
